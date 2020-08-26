424fea217c4e5aa406525833cc5776c06b1b0045
Miji Kim <mkim0021@student.monash.edu>
2020-08-27 04:15:27 +1000

    Update navigation bar colour of active page

--- trailblazers/nutrient.php
+++ trailblazers/nutrient.php
@@ -1,3 +1,11 @@
+<?php
+$connect = mysqli_connect("localhost","root","","phpmyadmin");
+$query = "SELECT * FROM Food_nutrient";
+$result = mysqli_query($connect,$query);
+$data = array();
+
+
+?>
 <!DOCTYPE HTML>
 <!--
 	Ion by TEMPLATED
@@ -31,8 +39,8 @@
 						<li><a href="index.html">Home</a></li>
 						<li><a href="facts.html">Facts</a></li>
 						<li><a href="carbon_footprint.html">Carbon Footprint</a></li>
-						<li><a href="nutrient.php">Nutrient</a></li>
-                        <li><a href="about_us.html" class="active-page">About Us</a></li>
+						<li><a href="nutrient.php" class="active-page">Nutrient</a></li>
+                        <li><a href="about_us.html">About Us</a></li>
 					</ul>
 				</nav>
 			</header>
@@ -40,43 +48,37 @@
 		<!-- Main -->
 			<section id="main" class="wrapper style1">
 				<header class="major">
-					<h2>About Us</h2>
-					<p>We are Trailblazers</p>
+					<h2>Nutrient</h2>
+					<p>Find out nutrition information</p>
 				</header>
 				<div class="container">
-					<div class="row">
-						<div class="4u">
-							<section>
-								<h3>Magna massa blandit</h3>
-								<p>Feugiat amet accumsan ante aliquet feugiat accumsan. Ante blandit accumsan eu amet tortor non lorem felis semper. Interdum adipiscing orci feugiat penatibus adipiscing col cubilia lorem ipsum dolor sit amet feugiat consequat.</p>
-								<ul class="actions">
-									<li><a href="#" class="button alt">Learn More</a></li>
-								</ul>
-							</section>
-							<hr />
-							<section>
-								<h3>Ante sed commodo</h3>
-								<ul class="alt">
-									<li><a href="#">Erat blandit risus vis adipiscing</a></li>
-									<li><a href="#">Tempus ultricies faucibus amet</a></li>
-									<li><a href="#">Arcu commodo non adipiscing quis</a></li>
-									<li><a href="#">Accumsan vis lacinia semper</a></li>
-									<li><a href="#">Erat blandit risus vis adipiscing</a></li>
-									<li><a href="#">Tempus ultricies faucibus amet</a></li>
-									<li><a href="#">Arcu commodo non adipiscing quis</a></li>
-									<li><a href="#">Accumsan vis lacinia semper</a></li>
-								</ul>
-							</section>
-						</div>
-						<div class="8u skel-cell-important">
-							<section>
-								<h2>Mollis ut adipiscing</h2>
-								<a href="#" class="image fit"><img src="images/team.jpg" alt="" /></a>
-								<p>Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo. Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
-								<p>Amet nibh adipiscing adipiscing. Commodo ante vis placerat interdum massa massa primis. Tempus condimentum tempus non ac varius cubilia adipiscing placerat lorem turpis at. Aliquet lorem porttitor interdum. Amet lacus. Aliquam lobortis faucibus blandit ac phasellus. In amet magna non interdum volutpat porttitor metus a ante ac neque. Nisi turpis. Commodo col. Interdum adipiscing mollis ut aliquam id ante adipiscing commodo integer arcu amet blandit adipiscing arcu ante.</p>
-							</section>
-						</div>
-					</div>
+					<section>
+						<h2>Nutrient per food group</h2>
+						<div id="nutrient" style="width:900px; height:500px;">
+
+                            <?php
+                                $selected = "Calcium";
+                                $options = array('Calcium','Carb','Fat','Fiber','Protein','Sugar','Vitamin A','Vitamin C', 'Vitamin E');
+                                
+                                echo "<select>";                                
+                                foreach($options as $option) {
+                                    
+                                    if($selected == $option) {
+                                         echo "<option selected ='selected' value='$option'>$option</option>";
+                                    }
+                                    else {
+                                         echo "<option value='$option'>$option</option>";
+                                    }
+                                   
+                                }
+                                echo "<select>";
+                            ?>
+                        </div>
+                        
+                        <a href="#" class="image fit"><img src="images/banner.jpg" alt="" /></a>
+						<p>Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo.</p>
+						<p>Amet nibh adipiscing adipiscing. </p>
+					</section>
 					<hr class="major" />
 					<div class="row">
 						<div class="6u">
